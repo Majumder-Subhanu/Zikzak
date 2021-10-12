34a155c19559d5750b9b80959d1c074960328684
Subhanu <subhanu.majumder@theheritageschool.net.in>
2021-10-12 22:15:49 +0530

    Zikzak v - 1.0 [Working]

---
+++ app/src/main/java/com/company/zikzak/MainActivity.java
@@ -0,0 +1,35 @@
+package com.company.zikzak;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://zikzak.in");
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
